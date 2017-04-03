aea850f86982d4bfa58a0c918255a4eb1e4f8ae8
Aimurat Torebaev <aymurat_kz@mail.ru>
2017-04-04 01:50:52 +0600

    test

---
+++ CodeReview_Firstname.cs
@@ -0,0 +1,34 @@
+ѕровер€л методы исход€ из синтаксиса Java, поэтому ниже перечисленные синтаксические ошибки возможно 
+могут быть вовсе не ошибками по C#  
+ћетод Parse
+1. —интаксические ошибки в коде:
+- Parse(object o) - неправильно написан тип аргумента, правильно будет Parse(Object o)
+- int.Parse(o.ToString()) - неправильное написание метода преобразовани€. 
+2. Ћогическа€
+- если случитс€ ошибка при преобразовании, метод Parse должен вернуть 0. ¬ данном коде проверка не осуществл€етс€
+
+ћетод Concat
+1. —интаксические ошибки в коде:
+- public string Concat(string[] lines) - неправильно написаны тип возвращаемого значени€ метода и тип аргумента 
+- string result - неправильно написан тип result
+- lines.Length - неправильный вызов метода length
+- string result - переменна€ не инициализирована
+
+
+ public int Parse(Object o) {
+        int integer = 0;
+        try{
+            integer = Integer.parseInt(o.toString());
+        }
+        catch (Exception e){
+        }
+        return integer;
+    }
+
+    public String Concat(String[] lines) {
+        String result = "";
+        for(int i = 0; i < lines.length; i++) {
+            result += lines[i];
+        }
+        return result;
+    }
